bdd7398de52342886fe07a1f4e7342319009a4e3
muiceking <scm1400@gmail.com>
2021-07-17 15:37:10 +0900

    no message

---
+++ src/main/java/gisapp/demo/TestController.java
@@ -0,0 +1,17 @@
+package gisapp.demo;
+
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/api/hello")
+    public HashMap hello() {
+        HashMap result = new HashMap();
+        result.put("message", "안녕하세요");
+
+        return result;
+    }
+}
